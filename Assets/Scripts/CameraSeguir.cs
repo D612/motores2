80aef861cd7828d617a15bdd209bdba72324856d
Deiverson <deiversonbh@gmail.com>
2020-03-14 16:09:25 -0300

    Aula 2 - Colisores - 11/03 - 13h30 as 15h10

---
+++ Assets/Scripts/CameraSeguir.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSeguir : MonoBehaviour
+{
+    //Velocidade do movimento de deslocamento da camera para o alvo
+    public float moveSpeed;
+
+    //Alvo que a camera vai seguir
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null) {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * moveSpeed);        
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public CharacterController control;
+    public Vector3 moveDir = Vector3.zero;
+    public float vel = 6;
+    public float gravidade = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Verificar se o jogador esta no chão
+        if (control.isGrounded) {
+            //Recebendo os valores de entrada do jogador
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //Atribuindo a posição do jogador (local) com a nova direção no mundo (world0
+            moveDir = transform.TransformDirection(moveDir);
+            //Adiionando a velocidadedo movimento
+            moveDir *= vel;
+        }
+        //Aplicando a gravidade durante o jogo
+        moveDir.y -= gravidade * Time.deltaTime;
+        //Executando a função para realizar o movimento com base nos novos valores de enrtada recebidos do vetor moveDir
+        control.Move(moveDir * Time.deltaTime);
+    }
+}
