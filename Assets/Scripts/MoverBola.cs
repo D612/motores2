e4b407842b3e82d64bdf18accb26b68b93330f92
Deiverson <deiversonbh@gmail.com>
2020-03-15 22:12:39 -0300

    - Testes finais com Colisores 3D e 2D (Pendente exemplo de Wheel Collider 3D)

---
+++ Assets/Scripts/MoverBola.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBola : MonoBehaviour
+{
+    public float moveForce;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * moveForce;
+        transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(h, 0));
+    }
+}
